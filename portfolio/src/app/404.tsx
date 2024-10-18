75e78932e545d4df375a4ff8d5caa70cea66bd08
EraldoOL <eo80797@gmail.com>
2024-10-18 20:36:16 -0300

    update 404.tsx

--- portfolio/src/app/404.tsx
+++ portfolio/src/app/404.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 const Notfound = () => {
   return(
     
-    <div>
+    <main>
     <h1>404</h1>
     <div>
     <p>Oops, não conseguimos encontrar</p>
     <span>clique no botão abaixo para voltar</span>
     </div>
     <Link href="/">Ir para pág inicial</Link>
-    </div>
+    </main>
     
     );
 };
