c844819195ac60f205812cf706e7489b1650d976
EraldoOL <eo80797@gmail.com>
2024-10-18 16:47:13 -0300

    update

---
+++ portfolio/src/app/404.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const notfound = () => {
+  return(
+    
+    <div>
+    <h1>404</h1>
+    <div>
+    <p>Oops, não conseguimos encontrar</p>
+    <spam>clique no botão abaixo para voltar</spam>
+    </div>
+    <Link href="/">
+    Ir para pág inicial
+    </Link>
+    </div>
+    
+    );
+};
+
+export default notfound;
