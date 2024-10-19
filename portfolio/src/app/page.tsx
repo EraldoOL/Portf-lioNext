13c5619a9b721e696066da1ad56feeb4969c3a00
EraldoOL <eo80797@gmail.com>
2024-10-19 16:17:33 -0300

    update page.tsx

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,11 +3,19 @@ import Link from "next/link";
 
 const Home = () => {
   return (
-
-      <main>
+    
+    <>
+    
+    <Head>
+    <title>Sobre mim | Eraldo</title>
+    </Head>
+    
+    <main>
       <h1>hello world</h1>
       <Link href="/contatos"> Ir para página contatos</Link>
-        </main>
+    </main>
+    </>
+ 
   );
 }
 
